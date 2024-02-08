6d2740926474e50ac0e1e2ff9ed3a7096bce56b6
Ricardo Jesus <ricardojesus189@gmail.com>
2024-02-08 22:58:50 +0000

    Imagem + descriçao + lista casas

--- TPC1/script1.py
+++ TPC1/script1.py
@@ -23,22 +23,53 @@ def percorrer_pastaXML(directory):
             
             rua_nome = None
             imagens = []
+            descricao = [] # Lista para armazenar as descrições
+            casas = [] # Lista para armazenar as informações das casas
+            
+            html_rua = template
             
             for rua in root.iter('nome'):
                 rua_nome = rua.text
                 
             for figura in root.iter('figura'):
                 imagem_path = figura.find('imagem').attrib['path']
+                imagem_path = f"../MapaRuas-materialBase/texto/{imagem_path}"
                 imagens.append(imagem_path)
                 
+            for para in root.iter('para'):
+                descricao.append(ET.tostring(para, encoding='unicode'))
+            
+            html_rua += "<div>"
+            html_rua += f"<h2>Informações sobre a rua:</h2>"
+            html_rua += "<br>"
+            html_rua += f"{descricao}"
+            html_rua += "</div>"
+            html_rua += "<br>"
+            
+            for casa in root.iter('casa'):
+                casa_info = {}
+                for child in casa:
+                    if child.tag == 'número':
+                        casa_info['número'] = child.text
+                    else:
+                        casa_info[child.tag] = child.text
+                casas.append(casa_info)
+            
+            html_rua += "<div>"
+            html_rua += f"<h2>Casas:</h2>"
+            html_rua += "<br>"
+            html_rua += f"{casas}"
+            html_rua += "</div>"
+            html_rua += "<br>"
+                
             if rua_nome:
-                html_rua = template
                 html_rua += f"<h1>{rua_nome}</h1>"
 
                 html_rua += "<div>"
                 
                 for imagem in imagens:
                     html_rua += f"<img src='{imagem}' alt='Imagem'>"
+                    print(imagem)
                 html_rua += "</div>"    
                 
                     
@@ -75,7 +106,3 @@ html += "</html>"
 file = open("ruas.html", "w", encoding="utf-8")
 file.write(html)
 file.close()
-
-    
-    
-    
